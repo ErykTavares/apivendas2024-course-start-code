5d666a7b9e5aeb05855bd38952e43586899b86cc
ErykTavares <erictavares35@gmail.com>
2025-04-24 21:50:30 -0300

    feat: add ApiError class and errorHandler middleware

---
+++ src/common/infrastructure/http/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import { ApiError } from '@/common/domain/errors/apiError';
+import { NextFunction, Request, Response } from 'express';
+
+export function errorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    _next: NextFunction,
+): Response {
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+}
+
+export default errorHandler;
