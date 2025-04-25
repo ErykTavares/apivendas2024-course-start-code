70f0097ac793ce24e3d72b21cfd5eb48e044fb66
ErykTavares <erictavares35@gmail.com>
2025-04-24 21:52:56 -0300

    feat(api): integrate errorHandler middleware

--- src/common/infrastructure/http/api.ts
+++ src/common/infrastructure/http/api.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import errorHandler from './middlewares/errorHandler';
+
 import routes from './routes';
 
 const api = express();
@@ -8,5 +10,6 @@ const api = express();
 api.use(cors());
 api.use(express.json());
 api.use(routes);
+api.use(errorHandler);
 
 export default api;
