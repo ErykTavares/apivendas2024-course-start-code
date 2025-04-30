adb8219b492ae013c79863972fff66efc1ee44f7
ErykTavares <erictavares35@gmail.com>
2025-04-30 10:30:25 -0300

    feat(api): add Swagger documentation setup and update server logs

--- src/common/infrastructure/http/api.ts
+++ src/common/infrastructure/http/api.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 import errorHandler from './middlewares/errorHandler';
 
 import routes from './routes';
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API STORE DOCUMENTATION',
+            version: '1.0.0',
+        },
+    },
+    apis: [],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
 const api = express();
 
 api.use(cors());
 api.use(express.json());
+api.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 api.use(routes);
 api.use(errorHandler);
 
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -6,4 +6,5 @@ const port = env.PORT;
 
 api.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    console.log('API docs available at GET /docs');
 });
