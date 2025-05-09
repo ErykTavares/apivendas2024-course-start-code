4742e33a6ba6eda30eaaae1b7445c6160c5064e0
ErykTavares <erictavares35@gmail.com>
2025-05-08 21:23:23 -0300

    feat(env): enhance environment variable configuration and database integration

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,10 +1,18 @@
 import { env } from '../env';
+import { dataSource } from '../typeorm';
 
 import api from './api';
 
 const port = env.PORT;
 
-api.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    console.log('API docs available at GET /docs');
-});
+dataSource
+    .initialize()
+    .then(() => {
+        api.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+            console.log('API docs available at GET /docs');
+        });
+    })
+    .catch(error => {
+        console.error('Error during Data Source initialization:', error);
+    });
