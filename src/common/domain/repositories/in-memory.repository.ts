b6147fdd9b5e6bad7c9d19e54be285fa199a2f21
ErykTavares <erictavares35@gmail.com>
2025-06-24 20:11:42 -0300

    feat(repository): implement InMemoryRepository methods

--- src/common/domain/repositories/in-memory.repository.ts
+++ src/common/domain/repositories/in-memory.repository.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from '../errors/not-found-error';
 import {
     RepositoryInterface,
     SearchInput,
@@ -20,26 +21,49 @@ export abstract class InMemoryRepository<Model extends ModelProps, CreateProps>
     sortableFields: string[] = [];
 
     create(props: CreateProps): Model {
-        throw new Error('Method not implemented.');
+        const model = {
+            id: crypto.randomUUID(),
+            create_at: new Date(),
+            update_at: new Date(),
+            ...props,
+        };
+
+        return model as unknown as Model;
     }
 
-    insert(modal: Model): Promise<Model> {
-        throw new Error('Method not implemented.');
+    async insert(model: Model): Promise<Model> {
+        this.items.push(model);
+        return model;
     }
 
-    findById(id: string): Promise<Model> {
-        throw new Error('Method not implemented.');
+    async findById(id: string): Promise<Model> {
+        return this._get(id);
     }
 
-    update(model: Model): Promise<Model> {
-        throw new Error('Method not implemented.');
+    async update(model: Model): Promise<Model> {
+        await this._get(model.id);
+        const index = this.items.findIndex(item => item.id === model.id);
+        this.items[index] = model;
+        return model;
     }
 
-    delete(id: string): Promise<void> {
-        throw new Error('Method not implemented.');
+    async delete(id: string): Promise<void> {
+        await this._get(id);
+        const index = this.items.findIndex(item => item.id === id);
+        this.items.splice(index, 1);
     }
 
     search(props: SearchInput): Promise<SearchOutput<Model>> {
         throw new Error('Method not implemented.');
+    }
+
+    protected async _get(id: string): Promise<Model> {
+        const model = this.items?.find(item => item?.id === id);
+
+        if (!model) {
+            throw new NotFoundError(`Model not found using ID ${id}.`);
+        }
+
+        return model;
     }
 }
