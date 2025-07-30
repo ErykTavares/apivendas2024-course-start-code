969cce7fb89ba12bd541ca6fa9f65b9da9bd9b5e
ErykTavares <erictavares35@gmail.com>
2025-07-29 21:00:40 -0300

    tests: applyFilter and applySort

--- src/common/domain/repositories/in-memory.repository.spec.ts
+++ src/common/domain/repositories/in-memory.repository.spec.ts
@@ -117,4 +117,124 @@ describe('InMemoryRepository Unit Tests', () => {
             expect(sut.items.length).toBe(0);
         });
     });
+
+    describe('applyFilter', () => {
+        it('should not filter items when the filter param is null', async () => {
+            const items = [model];
+            const spy = jest.spyOn(items, 'filter' as any);
+            const result = await sut['applyFilter'](items, null);
+
+            expect(spy).not.toHaveBeenCalled();
+            expect(result).toStrictEqual(items);
+        });
+
+        it('must filter the items using the filter param', async () => {
+            const items = [
+                {
+                    id: randomUUID(),
+                    name: 'test',
+                    price: 100,
+                    created_at,
+                    updated_at,
+                },
+                {
+                    id: randomUUID(),
+                    name: 'TEST',
+                    price: 200,
+                    created_at,
+                    updated_at,
+                },
+                {
+                    id: randomUUID(),
+                    name: 'user01',
+                    price: 300,
+                    created_at,
+                    updated_at,
+                },
+            ];
+            const spy = jest.spyOn(items, 'filter' as any);
+            let result = await sut['applyFilter'](items, 'test');
+
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(result).toStrictEqual([items[0], items[1]]);
+
+            result = await sut['applyFilter'](items, 'TEST');
+
+            expect(spy).toHaveBeenCalledTimes(2);
+            expect(result).toStrictEqual([items[0], items[1]]);
+
+            result = await sut['applyFilter'](items, 'no match');
+
+            expect(spy).toHaveBeenCalledTimes(3);
+            expect(result).toHaveLength(0);
+        });
+    });
+
+    describe('applySort', () => {
+        it('should not sort items', async () => {
+            const items = [
+                {
+                    id: randomUUID(),
+                    name: 'test',
+                    price: 100,
+                    created_at,
+                    updated_at,
+                },
+                {
+                    id: randomUUID(),
+                    name: 'TEST',
+                    price: 200,
+                    created_at,
+                    updated_at,
+                },
+                {
+                    id: randomUUID(),
+                    name: 'user01',
+                    price: 300,
+                    created_at,
+                    updated_at,
+                },
+            ];
+            let result = await sut['applySort'](items, null, null);
+
+            expect(result).toStrictEqual(items);
+
+            result = await sut['applySort'](items, 'id', null);
+
+            expect(result).toStrictEqual(items);
+        });
+
+        it('must sort items using the name', async () => {
+            const items = [
+                {
+                    id: randomUUID(),
+                    name: 'user02',
+                    price: 100,
+                    created_at,
+                    updated_at,
+                },
+                {
+                    id: randomUUID(),
+                    name: 'user03',
+                    price: 200,
+                    created_at,
+                    updated_at,
+                },
+                {
+                    id: randomUUID(),
+                    name: 'user01',
+                    price: 300,
+                    created_at,
+                    updated_at,
+                },
+            ];
+            let result = await sut['applySort'](items, 'name', 'desc');
+
+            expect(result).toStrictEqual([items[1], items[0], items[2]]);
+
+            result = await sut['applySort'](items, 'name', 'asc');
+
+            expect(result).toStrictEqual([items[2], items[0], items[1]]);
+        });
+    });
 });
