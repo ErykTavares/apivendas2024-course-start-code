edff1580bbd4b85c505cf4f540c69ee5d763b7a2
ErykTavares <erictavares35@gmail.com>
2025-09-11 21:48:46 -0300

    feat(products): implement applyFilter and applySort methods in unit tests

--- src/products/infrastructure/in-memory/repositories/products-in-memory-repository.spec.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory-repository.spec.ts
@@ -53,4 +53,74 @@ describe('ProductsInMemoryRepository Unit Tests', () => {
             await sut.conflictingNames('Test Product');
         });
     });
+
+    describe('applyFilter', () => {
+        it('should not filter items when the filter param is null', async () => {
+            const data = productsDataBuilder();
+            sut.items.push(data);
+
+            const spy = jest.spyOn(sut.items, 'filter' as any);
+            const result = await sut['applyFilter'](sut.items, null);
+
+            expect(spy).not.toHaveBeenCalled();
+            expect(result).toStrictEqual(sut.items);
+        });
+
+        it('must filter the items using the filter param', async () => {
+            const items = [
+                productsDataBuilder({ name: 'test' }),
+                productsDataBuilder({ name: 'TEST' }),
+                productsDataBuilder({ name: 'fake' }),
+            ];
+
+            sut.items.push(...items);
+
+            const spy = jest.spyOn(sut.items, 'filter' as any);
+            let result = await sut['applyFilter'](sut.items, 'test');
+
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(result).toStrictEqual([items[0], items[1]]);
+        });
+    });
+
+    describe('applySort', () => {
+        it('should sort items by created_at when no sort parameters are provided', async () => {
+            const created_at = new Date();
+
+            const items = [
+                productsDataBuilder({ name: 'test', created_at: created_at }),
+                productsDataBuilder({
+                    name: 'TEST',
+                    created_at: new Date(created_at.getTime() + 100),
+                }),
+                productsDataBuilder({
+                    name: 'fake',
+                    created_at: new Date(created_at.getTime() + 200),
+                }),
+            ];
+
+            sut.items.push(...items);
+
+            const result = await sut['applySort'](sut.items, null, null);
+
+            expect(result).toStrictEqual([items[2], items[1], items[0]]);
+        });
+
+        it('must sort items using the name', async () => {
+            const items = [
+                productsDataBuilder({ name: 'c' }),
+                productsDataBuilder({
+                    name: 'a',
+                }),
+                productsDataBuilder({
+                    name: 'b',
+                }),
+            ];
+
+            sut.items.push(...items);
+
+            const result = await sut['applySort'](sut.items, 'name', 'desc');
+            expect(result).toStrictEqual([items[0], items[2], items[1]]);
+        });
+    });
 });
