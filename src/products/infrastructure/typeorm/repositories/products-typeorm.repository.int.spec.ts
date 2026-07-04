40e75cf3b319f0231ddf11f5005c7084b41c63ff
Eryk Tavares <EricTavares35@gmail.com>
2026-07-03 23:41:09 -0300

    feat(tests): enhance integration tests for ProductsTypeormRepository

--- src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int.spec.ts
+++ src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int.spec.ts
@@ -43,4 +43,49 @@ describe('ProductsTypeormRepository Integration Tests', () => {
             expect(res.name).toEqual(product.name);
         });
     });
+
+    describe('create', () => {
+        it('should create a new product object', () => {
+            const data = productsDataBuilder({
+                name: 'Product 1',
+            });
+            const result = ormRepository.create(data);
+
+            expect(result.name).toEqual(data.name);
+        });
+    });
+
+    describe('insert', () => {
+        it('should insert a new product', async () => {
+            const data = productsDataBuilder({
+                name: 'Product 1',
+            });
+            const result = await ormRepository.insert(data);
+
+            expect(result.name).toEqual(data.name);
+        });
+    });
+
+    describe('update', () => {
+        it('should generate an error when the product is not found', async () => {
+            const data = productsDataBuilder({});
+
+            await expect(ormRepository.update(data)).rejects.toThrow(
+                new NotFoundError(`Product not found using ID ${data.id}`),
+            );
+        });
+
+        it('should update a product', async () => {
+            const data = productsDataBuilder({});
+            const product = testDataSource.manager.create(Product, data);
+
+            await testDataSource.manager.save(product);
+
+            product.name = 'Product 1 Updated';
+
+            const res = await ormRepository.update(product);
+
+            expect(res.name).toEqual('Product 1 Updated');
+        });
+    });
 });
