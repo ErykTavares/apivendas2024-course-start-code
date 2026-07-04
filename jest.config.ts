45416fa972523153ea15cd84f0a017a58c44c527
Eryk Tavares <EricTavares35@gmail.com>
2026-07-03 23:28:06 -0300

    fix(config): update jest and tsconfig configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
+import { createRequire } from 'node:module';
 import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+
+const require = createRequire(import.meta.url);
+const { compilerOptions } = require('./tsconfig.json');
 
 export default {
     moduleFileExtensions: ['js', 'ts', 'json'],
